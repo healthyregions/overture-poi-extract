07b0170d337c6faa77fefb4d67ca76fac084e1b7
Adam Cox <mr.adamcox@gmail.com>
2025-03-06 09:56:08 -0600

    initial port of script from OEPS and update README

--- README.md
+++ README.md
@@ -1,2 +1,175 @@
 # overture-poi-extract
-A simple script for creating geojson, shp, and pmtiles extracts from the Overture Maps POI dataset.
+
+A standalone script for creating geojson, shp, and pmtiles extracts from the Overture Maps POI dataset, specifically for  that uses GeoPandas and DuckDB.
+
+## Pre-made layers
+
+The following extracts have been created for use as independent data overlays within the SDOH & Place Project's Data Discovery App. Without any filter geometry provided, they default to using the full US geometry in this repo.
+
+#### Grocery/Supermarkets
+
+```
+python ./extract_pois.py \
+    -c asian_grocery_store \
+    -c ethical_grocery \
+    -c frozen_foods \
+    -c grocery_store \
+    -c international_grocery_store \
+    -c korean_grocery_store \
+    -c kosher_grocery_store \
+    -c mexican_grocery_store \
+    -c organic_grocery_store \
+    -c specialty_grocery_store \
+    -c supermarket \
+    -c wholesale_grocer \
+    -o output/us-grocery.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
+
+#### Schools
+
+```
+python ./extract_pois.py \
+    -c charter_school \
+    -c day_care_preschool \
+    -c education \
+    -c elementary_school \
+    -c high_school \
+    -c middle_school \
+    -c montessori_school \
+    -c preschool \
+    -c private_school \
+    -c public_school \
+    -c religious_school \
+    -c school \
+    -c school_district_offices \
+    -c school_sports_league \
+    -c school_sports_team \
+    -c vocational_and_technical_school \
+    -o output/us-schools.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
+
+#### Airports
+
+```
+python ./extract_pois.py \
+    -c airport \
+    -c airport_lounge \
+    -c airport_shuttles \
+    -c airport_terminal \
+    -o output/us-airports.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
+
+#### Parks
+
+```
+python ./extract_pois.py \
+    -c campground \
+    -c dog_park \
+    -c hiking_trail \
+    -c national_park \
+    -c park \
+    -c playground \
+    -c state_park \
+    -o output/us-parks.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
+
+#### Exercise/Gyms
+
+```
+python ./extract_pois.py \
+    -c boxing_club \
+    -c boxing_gym \
+    -c brazilian_jiu_jitsu_club \
+    -c chinese_martial_arts_club \
+    -c cricket_ground \
+    -c cycling_classes \
+    -c fencing_club \
+    -c golf_club \
+    -c golf_course \
+    -c gym \
+    -c gymnastics_center \
+    -c health_and_wellness_club \
+    -c hiking_trail \
+    -c karate_club \
+    -c kickboxing_club \
+    -c martial_arts_club \
+    -c muay_thai_club \
+    -c pilates_studio \
+    -c racquetball_court \
+    -c soccer_club \
+    -c soccer_field \
+    -c sports_club_and_league \
+    -c squash_court \
+    -c swimming_pool \
+    -c taekwondo_club \
+    -c tai_chi_studio \
+    -c tennis_court \
+    -c volleyball_court \
+    -o output/us-exercise.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
+
+#### Libraries
+
+```
+python ./extract_pois.py \
+    -c library \
+    -o output/us-libraries.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
+
+#### Child Enrichment
+
+```
+python ./extract_pois.py \
+    -c after_school_program \
+    -c child_care_and_day_care \
+    -c children's_museum \
+    -c day_care_preschool \
+    -c educational_camp \
+    -c library \
+    -c music_school \
+    -c private_tutor \
+    -c tutoring_center \
+    -o output/us-child-enrichment.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
+
+#### Adult Education
+
+```
+python ./extract_pois.py \
+    -c adoption_services \
+    -c art_school \
+    -c bartending_school \
+    -c business_schools \
+    -c college_university \
+    -c cooking_classes \
+    -c cooking_school \
+    -c cosmetology_school \
+    -c dance_school \
+    -c dentistry_schools \
+    -c drama_school \
+    -c driving_school \
+    -c dui_school \
+    -c engineering_schools \
+    -c flight_school \
+    -c language_school \
+    -c library \
+    -c massage_school \
+    -c medical_school \
+    -c medical_sciences_schools \
+    -c music_school \
+    -c nursing_school \
+    -c ski_and_snowboard_school \
+    -c specialty_school \
+    -c sports_school \
+    -c surfing_school \
+    -c traffic_school \
+    -o output/us-adult-education.pmtiles \
+    --tippecanoe-path /opt/tippecanoe/tippecanoe
+```
---
+++ extract_pois.py
@@ -0,0 +1,305 @@
+import uuid
+from datetime import datetime
+from pathlib import Path
+from argparse import Namespace
+import subprocess
+
+import click
+import duckdb
+import geopandas as gpd
+import pandas as pd
+from shapely import bounds, from_geojson
+
+cb_lookup = {
+    "040": "https://herop-geodata.s3.us-east-2.amazonaws.com/oeps/state-2018-500k-shp.zip",
+    "050": "https://herop-geodata.s3.us-east-2.amazonaws.com/oeps/county-2018-500k-shp.zip",
+    "140": "https://herop-geodata.s3.us-east-2.amazonaws.com/oeps/tract-2018-500k-shp.zip",
+    "150": "https://herop-geodata.s3.us-east-2.amazonaws.com/oeps/bg-2018-500k-shp.zip",
+    "860": "https://herop-geodata.s3.us-east-2.amazonaws.com/oeps/zcta-2018-500k-shp.zip",
+}
+
+
+def get_connection():
+    connection = duckdb.connect()
+
+    # these props must be set to empty before the query to s3, otherwise it fails. See:
+    # https://github.com/duckdb/duckdb/issues/7970#issuecomment-2118343680
+    connection.execute("SET s3_access_key_id='';SET s3_secret_access_key='';")
+
+    connection.install_extension("spatial")
+    connection.install_extension("httpfs")
+    connection.load_extension("spatial")
+    connection.load_extension("httpfs")
+
+    return connection
+
+
+def get_filter_geometry(input_file: str, filter_value: str):
+    print(input_file)
+
+    gdf = gpd.read_file(input_file)
+    filter_field, filter_value = filter_value.split("=")
+
+    filter_row = gdf[gdf[filter_field] == filter_value]
+
+    return filter_row["geometry"].values[0]
+
+def get_herop_geometry(herop_ids: list[str]):
+
+    prefix = herop_ids[0][:3]
+    gdf = gpd.read_file(cb_lookup[prefix])
+    rows = gdf[gdf["HEROP_ID"].isin(herop_ids)]
+    dissolved = rows.geometry.unary_union
+    return dissolved
+
+def get_full_us_filter():
+
+    ## this code was first used to create the geojson, and then I manually
+    ## edited the geojson to convert from 100 polygons to 7 multipolygons,
+    ## grouped across the world. Load this edited file here.
+
+    # gdf = gpd.read_file(cb_lookup["040"])
+    # dissolved = gdf.geometry.buffer(0.1).unary_union.buffer(-0.1)
+    # with open("us-geom.geojson", "w") as o:
+    #     o.write(to_geojson(dissolved, indent=1))
+
+    with open("full-us-geoms-dissolved.geojson", "r") as o:
+        geojson = from_geojson(o.read())
+
+    return geojson.geoms
+
+def get_data(
+    geometry_filters: list = [],
+    categories: list = [],
+    confidence: str = ".8",
+):
+    con_clause = f"confidence >= {confidence} AND" if confidence != "-1" else ""
+    print(f"confidence filter: {con_clause}")
+
+    cat_clause = (
+        "category IN ('{}') AND".format("', '".join(categories)) if categories else ""
+    )
+    print(f"category filter: {cat_clause}")
+
+    gdfs = []
+    for geom in geometry_filters:
+
+        bbox = bounds(geom)
+
+        overture_url = (
+            "s3://overturemaps-us-west-2/release/2023-11-14-alpha.0/theme=places/type=*/*"
+        )
+
+        query_sql = """SELECT
+            FILTER(names.common, x->x.language = 'local') [ 1 ].value as name,
+            categories.main as category,
+            addresses[1].freeform as address,
+            addresses[1].locality as city,
+            addresses[1].postcode as zip,
+            addresses[1].region as state,
+            ROUND(confidence,2) as confidence,
+            ST_AsText(geometry) as wkt
+        FROM read_parquet('{}', filename=true, hive_partitioning=1)
+        WHERE
+            {}
+            {}
+            bbox.minX BETWEEN {} AND {} AND
+            bbox.minY BETWEEN {} AND {}
+            """.format(
+            overture_url,
+            con_clause,
+            cat_clause,
+            bbox[0],
+            bbox[2],
+            bbox[1],
+            bbox[3],
+        )
+
+        print(query_sql)
+
+        start = datetime.now()
+        connection = get_connection()
+        df = connection.execute(query_sql).df()
+        print(datetime.now() - start)
+
+        # strip the extra 4 from zip codes
+        df["zip"] = df["zip"].str[:5]
+
+        gs = gpd.GeoSeries.from_wkt(df["wkt"])
+        gdf = gpd.GeoDataFrame(df, geometry=gs, crs="EPSG:4326")
+
+        del gdf["wkt"]
+
+        print(f"{len(gdf)} rows returned")
+
+        gdf = gdf.clip(gpd.GeoSeries(geom, crs="EPSG:4326"))
+
+        gdfs.append(gdf)
+
+    return pd.concat(gdfs)
+
+def write_output(gdf: gpd.GeoDataFrame, outpath: Path, tippecanoe_path:str=None):
+
+    if outpath.suffix == ".geojson":
+        gdf.to_file(outpath, driver="GeoJSON")
+    elif outpath.suffix == ".shp":
+        gdf.to_file(outpath)
+    elif outpath.suffix == ".pmtiles":
+        json_path = str(outpath).replace(".pmtiles", ".geojson")
+        print(json_path)
+        gdf.to_file(json_path, driver="GeoJSON")
+        cmd = [
+            tippecanoe_path,
+            # "-zg",
+            # tried a lot of zoom level directives, and seems like for block group
+            # (which I believe is the densest)shp_paths 10 is needed to preserve shapes well enough.
+            "-z10",
+            "--cluster-distance",
+            "10",
+            "--cluster-maxzoom",
+            "g",
+            "-r1",
+            "-d",
+            "20",
+            "--projection",
+            "EPSG:4326",
+            "-o",
+            str(outpath),
+            "-l",
+            "resources",
+            "--force",
+            str(json_path),
+        ]
+        print(cmd)
+        subprocess.run(cmd)
+
+    return outpath
+
+
+@click.command("get-pois")
+@click.option(
+    "--categories",
+    "-c",
+    multiple=True,
+    help="The exact name of one or more categories to include in the query. If not provided, all points "
+    "will be included in the export.",
+)
+@click.option(
+    "--outfile",
+    "-o",
+    help="Path to output file. If not provided, a small preview of the query result will be printed to "
+    "the console.",
+    type=click.Path(
+        resolve_path=True,
+        path_type=Path,
+    ),
+)
+@click.option(
+    "--confidence",
+    default=".8",
+    help="level of confidence to use when querying Overture data (greater than or equal to)",
+)
+@click.option(
+    "-g", "--geometry-ids",
+    multiple=True,
+    help="One or more HEROP_IDs which will be matched against prepared Census Boundary files to acquire a geometry filter",
+)
+@click.option(
+    "--filter-file",
+    help="Geospatial dataset with geometry to filter against. Can be a shapefile or geojson dataset, either "
+    "a local path or a url to one stored in S3.",
+)
+@click.option(
+    "--filter-unit",
+    help="Field=Value for a single feature to find in the filter-file and use as a geometry filter in the query.",
+)
+@click.option(
+    "--export-category-list",
+    is_flag=True,
+    default=False,
+    help="Export a list of all categories included in the query to a CSV file. Only really useful if "
+    "you don't include any categories in the filter.",
+)
+@click.option(
+    "--separate-files",
+    is_flag=True,
+    default=False,
+    help="Write separate file for each category in the results.",
+)
+@click.option(
+    "--tippecanoe-path",
+    default=None,
+    help="Path to tippecanoe binary needed for conversion to PMTiles.",
+)
+@click.option(
+    "--upload", is_flag=True, default=False, help="Upload the output file to S3."
+)
+@click.option(
+    "--upload-prefix", is_flag=True, default=False, help="Upload the output file to S3."
+)
+def get_pois(**kwargs):
+    """This operation will query the Overture Places (a.k.a. Point of Interest) dataset and extract all points
+    matching the specified categories that fall within the provided spatial boundary.
+
+    Example:
+
+    ```
+    flask overture get-pois --filter-file "https://herop-geodata.s3.us-east-2.amazonaws.com/place-2018.shp" -c hospital --filter-unit 3651000
+    ```
+    """
+    args = Namespace(**kwargs)
+
+    ## 0) INITIAL ARGS VALIDATION
+
+    if args.geometry_ids:
+        prefixes = set([i[:3] for i in args.geometry_ids])
+        if len(prefixes) > 1:
+            raise Exception("Can only ids with the same prefixes.")
+        if list(prefixes)[0] not in cb_lookup:
+            raise Exception("Invalid prefix on HEROP_ID:", args.geometry_ids[0][:3])
+    if args.filter_file or args.filter_unit:
+        if not args.filter_file or not args.filter_unit:
+            raise Exception("--filter-file and --filter-unit must both be provided if one is to be used.")
+    if (
+        args.outfile and Path(args.outfile).suffix == ".pmtiles"
+    ) and args.tippecanoe_path is None:
+        raise Exception("Tippecanoe path needed for PMTiles output.")
+
+
+    ## 1) CREATE GEOMETRY FILTER BASED ON INPUT
+
+    if args.geometry_ids:
+        filter_geoms = [get_herop_geometry(args.geometry_ids)]
+    elif args.filter_file or args.filter_unit:
+        filter_geoms = [get_filter_geometry(args.filter_file, args.filter_unit)]
+    else:
+        filter_geoms = get_full_us_filter()
+
+    ## 2) QUERY FOR POI DATA
+    data = get_data(
+        geometry_filters=filter_geoms,
+        categories=args.categories,
+        confidence=args.confidence,
+    )
+
+    ## 3) (optional) WRITE OUT A LIST OF ALL RETURNED CATEGORIES
+    if args.export_category_list:
+        # Get the distinct values of the column
+        distinct_values = data["category"].drop_duplicates()
+
+        fpath = (
+            args.outfile.name + "__categories.csv"
+            if args.outfile
+            else f"categories__{uuid.uuid4().hex}.csv"
+        )
+
+        # Write the distinct values to a CSV file
+        distinct_values.to_csv(fpath, index=False, header=None)
+
+    ## 4) (optional) WRITE DATA TO OUTPUT FILE(S)
+    if args.outfile:
+        outpath = write_output(data, args.outfile, args.tippecanoe_path)
+        print(f"saved to: {outpath}")
+
+if __name__ == "__main__":
+    get_pois()
